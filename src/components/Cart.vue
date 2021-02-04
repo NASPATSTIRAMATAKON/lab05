1d8aa9017b31211a2447a4736399464caec6d6b5
DESKTOP-2AH228N\KeeRong <614234012@parichat.skru.ac.th>
2021-02-04 08:48:04 +0700

    4/02/64

---
+++ src/components/Cart.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <h2>Books in Cart</h2>
+    <div v-bind:key="book.bookid" v-for="book in cart">
+        <CartItem v-bind:book="book" v-on:removeCartitem="Remove(book.bookid)" />
+    </div>
+</div>
+</template>
+
+<script>
+import CartItem from './CartItem.vue';
+export default {
+    name: "Cart",
+    props: ["cart"],
+    components: {
+        CartItem
+    },
+    methods:{
+        Remove(bookId){
+            this.$emit('removeCartitem',bookId);
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.book-item {
+    background: #a1b5ca;
+    padding: 10px;
+    border-bottom: 1px rgb(230, 193, 193) dotted;
+}
+</style>
